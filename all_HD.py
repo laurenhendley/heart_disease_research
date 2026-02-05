9d36c8591b4281a7834a504c514610d066829647
laurenhendley <lauren.hendley4@gmail.com>
2026-02-05 18:26:56 -0500

    Basic files

--- all_HD.py
+++ all_HD.py
@@ -1,9 +1,22 @@
 """
+Finding common symptoms and predictors for heart disease across all sexes in the dataset.
+Model types used:
+-
+-
 
 
 @author: Lauren Hendley
 @author: Sayohn David
 """
 
+
+
+'''
+1. Investigation- look @ dataset, find the parameters
+2. Model development - model fitting, train/test split, model prediction
+    - Use different models (random forests, support vector machines, SCARF, etc)
+3. Find model results
+4. Compare accuracies of the different models (see which is most accurate)
+'''
 
 
