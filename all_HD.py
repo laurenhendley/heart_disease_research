570ec86f33f01e7b682fed68d67daf780ec8491b
laurenhendley <lauren.hendley4@gmail.com>
2026-02-08 20:55:26 -0500

    Initial planning

--- all_HD.py
+++ all_HD.py
@@ -20,3 +20,41 @@ Model types used:
 '''
 
 
+
+
+'''
+Methods required:
+1. Main method
+- Train/test split for each model
+2. Dataset breakdown
+3. Data preprocessing
+4. Comparison of models (AUC, acccuracy, precision, recall, F1 score)
+
+1. Logistic Regression
+2. L1/L2/ElasticNet
+3. Random Forest
+4. XGBoost
+'''
+
+
+
+# IMPORTS
+import pandas as pd
+from sklearn.metrics import accuracy_score, recall_score
+
+
+## Evaluate markers in the dataset
+def dataset_eval(ds):
+    ds.head()
+
+    print("Dataset shape: ", ds.shape)
+
+    print("\nDataset information: ", ds.info())
+
+    print("\nDataset description: ", ds.describe())
+
+
+# Main method
+if __name__ == 'main':
+    ds = pd.read_csv("/kaggle/input/heart-disease-dataset/heart.csv")
+    dataset_eval(ds)
